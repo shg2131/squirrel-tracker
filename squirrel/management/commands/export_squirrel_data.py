4fdee5e289f03a00f68726d073b4e77e01791614
Simon Gee <shg2131@columbia.edu>
2020-10-08 04:22:17 +0000

    Implement export_squirrel_data
    Not sure what data needs to be exported by export_squirrel_data
    Also made small changes to import_squirrel data

--- squirrel/management/commands/export_squirrel_data.py
+++ squirrel/management/commands/export_squirrel_data.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 import pandas as pd
 
 class Command(BaseCommand):
-    help = 'Imports data from City of New York squirrel census'
+    help = 'Exports squirrel data to a given file path'
     
     def add_arguments(self, parser):
         parser.add_argument('path', nargs='+', type=str)
@@ -11,11 +11,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if len(options['path']) != 1:
             self.stdout.write(
-                self.style.ERROR(f'Usage: python manage.py import_squirrel_data /path/to/file.csv')
+                self.style.ERROR(f'Usage: python manage.py export_squirrel_data /path/to/file.csv')
             )
             return 
         path = options['path'][0]
-        url = 'https://data.cityofnewyork.us/api/views/vfnx-vebw/rows.csv'
-        pd.read_csv(url).to_csv(path)
+        data = pd.DataFrame() # not sure exactly what data we're supposed to export??
+        data.to_csv(path)
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully imported squirrel data to {path}!'))
+        self.stdout.write(self.style.SUCCESS(f'Successfully exported squirrel data to {path}!'))
