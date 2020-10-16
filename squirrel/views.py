3560c5e262e68e0edadb9f20ea57e5914cf941f8
Simon Gee <shg2131@columbia.edu>
2020-10-16 23:14:52 +0000

    Implement stats view and html

--- squirrel/views.py
+++ squirrel/views.py
@@ -37,7 +37,37 @@ def sightings_detail(request, unique_squirrel_id):
 
 def stats(request):
     sightings = SquirrelSighting.objects.all()
+    
+    # Total numer of sightings
+    num_sightings = len(sightings)
+    avg_per_day = num_sightings / len(set([s.date for s in sightings]))
+    # avg longitude
+    # avg latitude
+    avg_long = sum([s.longitude for s in sightings]) / num_sightings
+    avg_lat = sum([s.latitude for s in sightings]) / num_sightings
+
+    # adult vs juvenile
+    adult_num = sum([1 if s.age == "Adult" else 0 for s in sightings])
+    juvenile_num = sum([1 if s.age == "Juvenile" else 0 for s in sightings])
+    other_num = num_sightings - adult_num - juvenile_num
+
+    # % AM vs PM
+    am_pct = sum([1 if s.shift == "AM" else 0 for s in sightings]) / num_sightings * 100
+    pm_pct = sum([1 if s.shift == "PM" else 0 for s in sightings]) / num_sightings * 100
+    na_pct = 100 - am_pct - pm_pct
+
+    # % Approaches vs Indifferent vs Runs From
+    # Line graph with how many spotted on each day
+
     context = {
-        'sightings': sightings,
+        'num_sightings': num_sightings,
+        'avg_per_day': round(avg_per_day, 2),
+        'avg_long': round(avg_long, 4),
+        'avg_lat': round(avg_lat, 4),
+        'adult_num': adult_num,
+        'juvenile_num': juvenile_num,
+        'other_num': other_num,
+        'am_pct': round(am_pct, 2),
+        'pm_pct': round(pm_pct, 2),
     }
     return render(request, 'squirrel/stats.html', context)
