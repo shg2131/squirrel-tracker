01807acd81443e497fc07c8128cf1cf696fb7fda
Simon Gee <shg2131@columbia.edu>
2020-10-16 17:08:04 +0000

    Cleanup code for sightings-list and sightings-detail

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.map, name='map'),
     path('sightings/', views.SightingsListView.as_view(), name='sightings-list'),
+    path('sightings/<str:unique_squirrel_id>/', views.sightings_detail, name='sightings-detail')
 ]
--- squirrel/views.py
+++ squirrel/views.py
@@ -23,3 +23,11 @@ class SightingsListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+def sightings_detail(request, unique_squirrel_id):
+    sighting = SquirrelSighting.objects.filter(unique_squirrel_id=unique_squirrel_id)[0]
+    print(sighting.unique_squirrel_id)
+    context = {
+        'sighting': sighting,
+    }
+    return render(request, 'squirrel/squirrelsighting_detail.html', context)
